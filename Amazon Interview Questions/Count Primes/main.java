1446addb0b82a819461c13fe7e0bb7c3b4f7cb6f
maryams9 <msoleimani89@gmail.com>
2020-04-13 20:53:55 -0600

    count primes solution is added

---
+++ Amazon Interview Questions/Count Primes/main.java
@@ -0,0 +1,24 @@
+//Question URL: https://leetcode.com/problems/count-primes/
+
+
+class Solution {
+    public int countPrimes(int n) {
+        if (n <=1 )return 0 ;
+
+        int primeCnt = 0 ;
+        boolean[] primes= new boolean[n] ;
+        for (int i =2; i< primes.length; i++) {
+            primes[i] = true;
+        }
+        for (int i=2; i*i < n ; i++) {
+            if (!primes[i])continue;
+            for (int j = i*i ; j<n ; j+= i) {
+                primes[j]= false;
+            }
+        }
+        for (int i =0 ; i < primes.length ; i++) {
+            if (primes[i]) primeCnt++;
+        }
+        return primeCnt;
+    }
+}
